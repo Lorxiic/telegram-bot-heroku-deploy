12e33e44df5481b94b5424e50e3e32a9a9ab381d
Lorxiic <48385637+Lorxiic@users.noreply.github.com>
2023-03-11 01:07:02 +0700

    Update config.php

--- config.php
+++ config.php
@@ -2,10 +2,10 @@
 
 $db_host = "us-cdbr-east-06.cleardb.net";
 
-$db_user = "b9d750177742e4";
+$db_user = "b62ccc370d47b5";
 
-$db_pass = "1b89c436";
+$db_pass = "5afeecdf";
 
-$db_name = "heroku_e8390792d8f98c7";
+$db_name = "heroku_a74d99c0cf8788f";
 
 $connect = mysqli_connect($db_host,$db_user,db_pass,$db_name) or die("database connection error");
